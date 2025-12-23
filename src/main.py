89bd2124dd1371c78c6ae8a48e27bd849c830931
Mohab Yasser <mohabhelp@gmail.com>
2025-12-24 01:14:53 +0200

    fix?

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from trainer import Trainer
 OmegaConf.register_new_resolver("eval", eval)
 
 
-@hydra.main(config_path="../config", config_name="trainer")
+@hydra.main(config_path="../config", config_name="trainer", version_base="1.1")
 def main(cfg: DictConfig):
     trainer = Trainer(cfg)
     trainer.run()
